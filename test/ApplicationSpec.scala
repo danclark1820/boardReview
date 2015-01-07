9051201b09f255cadabe293e41369d983782b87e
Dan Clark <dan.clark@quanttus.com>
2015-01-07 09:09:21 -0500

    add board list rendering test

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,6 +5,12 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
+import play.api.data.Form
+
+import models.Board
+import controllers.Application.BoardData
+import controllers.Application.boardForm
+
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
@@ -25,8 +31,25 @@ class ApplicationSpec extends Specification {
       contentType(home) must beSome.which(_ == "text/plain")
       contentAsString(home) must contain ("Hello")
     }
+    /***
+       This is where you left off. Trying to test rendering a list of boards, 
+       you were testing the template as a function.
+       Struggling with inputing the boardForm
+      */
+    "render a list of boards" in {
+      val brd = new Board(123456,"al merrick", "5'11''", "20''", "29.3", "2 1/2", "img_123.png")
+      val boardList = List(brd, brd, brd)
+      val boardIndex = views.html.index(boardList, boardForm)
+      contentAsString(boardIndex) must contain("al merrick")
+      contentAsString(boardIndex) must contain("3 board(s)")
+    }
 
-    "render the board page" in new WithApplication{
+    /***TODO
+    "delete a board from a list" in{
+    }
+     */
+
+    "render the boards page" in new WithApplication{
       val boards = route(FakeRequest(GET, "/boards")).get
       contentType(boards) must beSome.which(_ == "text/html") 
       status(boards) must equalTo(OK)
